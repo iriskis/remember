e916a05e34231a2933f20c8bfeec07d05f39cd4d
Shchukina Irina <irkis.iriskis@gmail.com>
2022-06-13 22:35:33 +0500

    Init first page

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid # Required for unique book instances
 
-# Create your models here.
+class Remember(models.Model):
+    user_id = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    comment = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return '%s' % (self.title)
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to="uploads/picture", blank=True, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.id, self.name)
--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import User, Remember
+
+def index(request):
+    num_users=User.objects.count()
+    num_remebers=Remember.objects.count()  
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_users':num_users, 'num_remebers':num_remebers},
+    )
