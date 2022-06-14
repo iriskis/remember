28a21572a996fa16d997bc2cc52085c614ff683d
Shchukina Irina <irkis.iriskis@gmail.com>
2022-06-14 20:23:10 +0500

    Add the field date for the model Remember

--- src/app/models.py
+++ src/app/models.py
@@ -5,6 +5,7 @@ class Remember(models.Model):
     author = models.CharField(max_length=100, null=True)
     title = models.CharField(max_length=200)
     comment = models.TextField(max_length=1000)
+    date = models.DateField(null=True)
 
     def __str__(self):
         return '%s' % (self.title)
